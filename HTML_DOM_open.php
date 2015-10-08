16f3afdfca2fa2184d846fe4dfcba1c5d705cf5f
Halil Barım <halilbarim@gmail.com>
2015-10-08 14:53:10 +0300

    Dom_Add_Files

---
+++ HTML_DOM_open.php
@@ -0,0 +1,33 @@
+<!DOCTYPE html>
+<html>
+<body>
+
+<p>Click the button to open an output stream, add some text, and close the output stream.</p>
+
+<button onclick="myFunction()">Try it</button>
+
+<script>
+function myFunction() {
+    document.open();
+    document.write("<h1>Hello World</h1>");
+    document.close();
+}
+</script>
+
+</body>
+</html>
+
+
+
+<?php 
+ 
+//sayfadaki herşey gider ekrana hello world yazar..
+//
+//
+//
+//
+  
+//HALİL_BARIM.
+
+
+?>
