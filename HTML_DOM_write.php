1741769ff5bf06fb95d213eec5b4ba83f1df318a
Halil Barım <halilbarim@gmail.com>
2015-10-08 16:35:48 +0300

    Dom_Add_FİLES

---
+++ HTML_DOM_write.php
@@ -0,0 +1,24 @@
+<!DOCTYPE html>
+<html>
+<body>
+
+<script>
+document.write("Hello World!");
+</script>
+
+</body>
+</html>
+
+
+<?php 
+ 
+//sayfaya (ekran) hello world yazar..
+//
+//
+//
+//
+  
+//HALİL_BARIM.
+
+
+?>
