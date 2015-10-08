41bc49b0ffc492d94ef67704377ad0a2c1920865
Halil Barım <halilbarim@gmail.com>
2015-10-08 14:47:08 +0300

    DOM ekleme

---
+++ HTML_DOM_normalizeDocument.php
@@ -0,0 +1,19 @@
+<script type="text/javascript">
+
+document.normalizeDocument();
+
+</script>
+
+<?php 
+ 
+//NormalizeDocument () yöntemi boş Metin düğümleri kaldırır ve komşu Metin düğümleri ekler.
+//Bu yöntem herhangi bir tarayıcıda desteklenmiyor, 
+//ancak normalize () yöntemiyle aynı çalışır.
+//
+//
+//
+  
+//HALİL_BARIM.
+
+
+?>
